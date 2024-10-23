009595022dc302f9346cf427be0d691cd138a421
fullstackjunkie <dirtroadddeveloper@gmail.com>
2024-10-23 18:21:40 -0400

    Finished this project

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
 const container = document.querySelector(".container");
+const inputText =  document.querySelector("#inText");
+const btn = document.querySelector("#btn");
 
-for(let i = 0; i < 16; i++) {
-    for(let j = 0; j < 16; j++) {
+btn.addEventListener("click", () => {
+    let size = inputText.value;
+    container.innerHTML = '';  
+    const cellSize = 100 / size;  
+
+    for(let i = 0; i < size * size; i++) {
         const cell = document.createElement("div");
-        cell.classList.add("cell");  // add a class for styling
+        cell.classList.add("cell");
+        cell.style.flex = `0 0 ${cellSize}%`;  
+        cell.style.height = `${cellSize}vh`;   
         container.appendChild(cell);
-
-        cell.addEventListener("mouseover", (e) => {
-            
-            e.target.style.backgroundColor = "black";
-
-        });
-
-        cell.addEventListener("mouseout", (e) => {
-
-            setTimeout(function() {
-                e.target.style.backgroundColor = "white";
-            }, 4000);
-            
-        });
+            cell.addEventListener("mouseover", (e) => {
+                
+                e.target.style.backgroundColor = "black";
+    
+            });
+    
+            cell.addEventListener("mouseout", (e) => {
+    
+                setTimeout(function() {
+                    e.target.style.backgroundColor = "white";
+                }, 2000);
+                
+            });
+        
     }
-}
+    
+});
+
+
+
+
+
